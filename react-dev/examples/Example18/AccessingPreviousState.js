7acfb05a6ccd70030f32d15692af0c7761f8977a
Wesley Ren <wren@walmartlabs.com>
2017-09-26 11:10:34 -0700

    added

---
+++ react-dev/examples/Example18/AccessingPreviousState.js
@@ -0,0 +1,28 @@
+var Counter = React.createClass({
+  getInitialState: function() {
+    return {
+      counter: 0
+    }
+  },
+
+  incrementCounter: function() {
+    this.setState(function(prevState, props) {
+      return {
+        counter: prevState.counter + props.increment
+      }
+    });
+  },
+
+  render: function() {
+    setTimeout(this.incrementCounter, 1000);
+    return (
+      <div>
+        <div className="box">
+          {this.state.counter}
+        </div>
+      </div>
+    );
+  }
+
+})
+ReactDOM.render(<Counter increment={5}/>, document.getElementById("outer"));
