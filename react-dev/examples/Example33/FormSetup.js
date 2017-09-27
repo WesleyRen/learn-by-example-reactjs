7251b8df6632864a1f672afcf78becd194fc23a3
Wesley Ren <wren@walmartlabs.com>
2017-09-27 15:16:21 -0700

    added select component with ES6 and Lambda

--- react-dev/examples/Example33/FormSetup.js
+++ react-dev/examples/Example33/FormSetup.js
@@ -4,9 +4,11 @@ class EmailForm extends React.Component {
     this.state = {
       email: '',
       name: '',
+      propertyType: 'condo',
     };
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handleNameChange = this.handleNameChange.bind(this);
+    this.handlePropertyTypeChange = this.handlePropertyTypeChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -16,10 +18,14 @@ class EmailForm extends React.Component {
   handleNameChange(name) {
     this.setState({name: name});
   }
+  handlePropertyTypeChange(propertyType) {
+    this.setState({propertyType: propertyType});
+  }
 
   handleSubmit(e) {
     console.log('Email submitted: ' + this.state.email);
     console.log('Name submitted: ' + this.state.name);
+    console.log('Property type submitted: ' + this.state.propertyType);
     e.preventDefault();
   }
   render() {
@@ -29,6 +35,9 @@ class EmailForm extends React.Component {
                     handleChange={this.handleEmailChange} />
         <TextInput  label={"Name"}
                     handleChange={this.handleNameChange} />
+        <SelectInput label={"Property Type"}
+                    options={this.props.propertyTypeOptions}
+                    handleChange={this.handlePropertyTypeChange} />
         <input className="input" type="submit" value="Submit"/>
       </form>
     );
@@ -58,5 +67,38 @@ class TextInput extends React.Component {
     )
   }
 }
+
+class SelectInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.handleChange(e.target.value);
+  }
+  render() {
+    return (
+      <div>
+        <div className="label">
+            {this.props.label}
+        </div>
+        <select value={this.props.value}
+                onChange={this.handleChange}>
+          {
+            this.props.options.map(
+              (option) => <option key={option}
+                                  value={option}>{option}</option>
+            )
+          }
+        </select>
+      </div>
+    )
+  }
+}
+
+EmailForm.defaultProps = {
+    propertyTypeOptions: ['condo', 'townhome', 'house']
+};
 
 ReactDOM.render(<EmailForm/>, document.getElementById("outer1"));
