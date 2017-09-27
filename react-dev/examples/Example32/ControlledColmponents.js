7a014967d7d7af42086e4fa97e704f96c95d26e8
Wesley Ren <wren@walmartlabs.com>
2017-09-27 10:53:05 -0700

    added form select option

--- react-dev/examples/Example32/ControlledColmponents.js
+++ react-dev/examples/Example32/ControlledColmponents.js
@@ -3,11 +3,15 @@ class EmailForm extends React.Component {
     super(props);
 
     this.state = {
-      emailValue: 'Some value',
+      emailValue: '',
+      nameValue: '',
+      propertyTypeValue: 'condo'
     };
 
     // by default "this" is not bound, which is null.
     this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handlePropertyTypeChange = this.handlePropertyTypeChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -16,8 +20,18 @@ class EmailForm extends React.Component {
     this.setState({emailValue: e.target.value});
   }
 
+  handleNameChange(e) {
+    this.setState({nameValue: e.target.value});
+  }
+
+  handlePropertyTypeChange(e) {
+    this.setState({propertyTypeValue: e.target.value});
+  }
+
   handleSubmit(e) {
     console.log('Email address submitted: ' + this.state.emailValue);
+    console.log('Name submitted: ' + this.state.nameValue);
+    console.log('Propert type submitted: ' + this.state.propertyTypeValue);
 
     e.preventDefault(); // Otherwise submittion will use native event handling.
   }
@@ -31,9 +45,26 @@ class EmailForm extends React.Component {
         <input className="input" type="text"
           value={this.state.emailValue}
           onChange={this.handleEmailChange}/>
-        <p>
+        <p className="input">
           {this.state.emailValue}
         </p>
+
+        <div className="label">
+          Name:
+        </div>
+        <textarea className="input"
+          value={this.state.nameValue}
+          onChange={this.handleNameChange} />
+
+        <div className="label">
+          Propert Type:
+        </div>
+        <select value={this.state.propertyTypeValue}
+          onChange={this.handlePropertyTypeChange} >
+          <option value="condo">Condo</option>
+          <option value="house">House</option>
+          <option value="townhome">Townhome</option>
+        </select>
         <input className="button" type="submit" value="Submit" />
       </form>
     )
